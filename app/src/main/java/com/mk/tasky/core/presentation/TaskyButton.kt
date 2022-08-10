cf7636d0be6c72bfe1c7bbb1b52caaf84a5e3dd2
Martin Kiperszmid <MKiperszmid@gmail.com>
2022-08-10 20:51:51 -0300

    Improved button corners

--- app/src/main/java/com/mk/tasky/core/presentation/TaskyButton.kt
+++ app/src/main/java/com/mk/tasky/core/presentation/TaskyButton.kt
@@ -9,7 +9,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.mk.tasky.ui.theme.Black
 import com.mk.tasky.ui.theme.White
 
@@ -17,6 +19,8 @@ import com.mk.tasky.ui.theme.White
 fun TaskyButton(
     text: String,
     onClick: () -> Unit,
+    fontSize: TextUnit = 16.sp,
+    fontWeight: FontWeight = FontWeight.Bold,
     modifier: Modifier = Modifier
 ) {
     Button(
@@ -26,12 +30,14 @@ fun TaskyButton(
             backgroundColor = Black,
             contentColor = White
         ),
-        shape = RoundedCornerShape(38.dp)
+        shape = RoundedCornerShape(if (text.length == 1) 16.dp else 38.dp)
     ) {
         Text(
             text = text,
             modifier = Modifier.padding(top = 8.dp, bottom = 8.dp),
-            fontWeight = FontWeight.Black
+            fontWeight = fontWeight,
+            maxLines = 1,
+            fontSize = fontSize
         )
     }
 }
