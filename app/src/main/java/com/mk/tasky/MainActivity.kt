af676b3cccfe2b2909692c92ba79e753754dab8c
Martin Kiperszmid <MKiperszmid@gmail.com>
2022-08-10 12:29:37 -0300

    Added Hilt

--- app/src/main/java/com/mk/tasky/MainActivity.kt
+++ app/src/main/java/com/mk/tasky/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.mk.tasky.ui.theme.TaskyTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/com/mk/tasky/MyApp.kt
@@ -0,0 +1,7 @@
+package com.mk.tasky
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MyApp : Application()
