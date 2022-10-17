098607437689d89560766707fe4bdec6db8d267d
Martin Kiperszmid <MKiperszmid@gmail.com>
2022-10-17 19:20:25 -0300

    Removed class variable for AlarmManager

--- app/src/main/java/com/mk/tasky/agenda/data/alarm/AlarmRegisterImpl.kt
+++ app/src/main/java/com/mk/tasky/agenda/data/alarm/AlarmRegisterImpl.kt
@@ -20,22 +20,20 @@ class AlarmRegisterImpl(
         const val ITEM_TYPE = "ITEM_TYPE"
     }
 
-    private lateinit var alarmManager: AlarmManager
-
     override fun updateAlarm(newItem: AgendaItem, previousItem: AgendaItem) {
         cancelAlarm(previousItem)
         setAlarm(newItem)
     }
 
     override fun cancelAlarm(item: AgendaItem) {
-        alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         val previousPendingIntent = getPendingIntent(item)
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         alarmManager.cancel(previousPendingIntent)
     }
 
     override fun setAlarm(item: AgendaItem) {
         if (System.currentTimeMillis() >= item.remindAt.toLong()) return
-        alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val alarmManager = context.getSystemService(Context.ALARM_SERVICE) as AlarmManager
         alarmManager.setExactAndAllowWhileIdle(
             AlarmManager.RTC_WAKEUP,
             item.remindAt.toLong(),
